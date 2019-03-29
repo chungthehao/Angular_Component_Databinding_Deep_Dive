287e1d03455056b7a1b77212d50e697fc8ea01c0
Chung Thế Hào <chungthehao@gmail.com>
2019-03-29 23:57:13 +0700

    Getting Access to ng-content with @ContentChild

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -19,6 +19,7 @@ OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
   @Input() name: string;
   @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { 
     console.log('constructor called!!!')
@@ -33,6 +34,8 @@ OnDestroy {
     console.log('ngOnInit called!!!')
 
     console.log('Text Content: ' + this.header.nativeElement.textContent)
+
+    console.log('Text Content of Paragraph: ' + this.paragraph.nativeElement.textContent)
   }
 
   ngDoCheck() {
@@ -41,6 +44,8 @@ OnDestroy {
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit called!')
+
+    console.log('Text Content of Paragraph: ' + this.paragraph.nativeElement.textContent)
   }
 
   ngAfterContentChecked() {
