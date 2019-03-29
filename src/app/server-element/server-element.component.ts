1135b7b013441947138a4ded38486fccc2f80c1c
Chung Thế Hào <chungthehao@gmail.com>
2019-03-29 21:33:17 +0700

    Projecting Content into Components with ng-content

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
-  encapsulation: ViewEncapsulation.Emulated // None, Native, ShadowDom
+  encapsulation: ViewEncapsulation.Emulated // None, Native, ShadowDom // Phạm vi ảnh hưởng của CSS
 })
 export class ServerElementComponent implements OnInit {
   // Dung Input decorator de bind data tu parent xuong component nay duoc.
